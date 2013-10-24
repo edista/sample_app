90592db2ec6ee4b8adfa218533b8d926fb1f7f70
Edi Surata <Edi@icehousecorp.com>
2013-10-24 15:21:11 +0700

    Add User microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only:[:create, :destroy]
   #get "users/new"
   resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
